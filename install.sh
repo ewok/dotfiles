69c341e307965443fc361088569c9b2a1e580293
Artur Taranchiev <ewok@ewok.ru>
2016-01-02 00:48:13 +0300

    Update installation script

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/resurrect
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+ln -s .tmux.con ~/.tmux.conf
+ln -s .zshrc ~/.zshrc
