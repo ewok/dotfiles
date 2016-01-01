1a9d9a91ae62a5e0998efbfb412e9145b46b87ab
Artur Taranchiev <ewok@ewok.ru>
2016-01-02 01:17:52 +0300

    Update installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/resurrect
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/tmux-plugins/tmux-resurrect $HOME/.tmux/resurrect
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
-ln -s .tmux.con ~/.tmux.conf
-ln -s .zshrc ~/.zshrc
+ln -s ${PWD}/.tmux.con $HOME/.tmux.conf
+ln -s ${PWD}/.zshrc $HOME/.zshrc
+ls -s ${PWD}/.vim $HOME/.vim
 
-ls -s .vim ~/.vim
-mkdir ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle
+mkdir $HOME/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle
