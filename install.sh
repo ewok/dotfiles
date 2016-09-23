d51197750ace7ef5a6d56242cfa60cfa823c76d4
Artur Taranchiev <ewok@ewok.ru>
2016-09-23 22:03:54 +0300

    Fix some error in installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+set -x
+
 git clone https://github.com/tmux-plugins/tmux-resurrect $HOME/.tmux/resurrect
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
-ln -s ${PWD}/.tmux.con $HOME/.tmux.conf
+ln -s ${PWD}/.tmux.conf $HOME/.tmux.conf
 ln -s ${PWD}/.zshrc $HOME/.zshrc
-ls -s ${PWD}/.vim $HOME/.vim
+ln -s ${PWD}/.vim $HOME/.vim
 
 mkdir $HOME/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle
