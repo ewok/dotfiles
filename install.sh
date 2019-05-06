ace96ac7d5c8246564836993fc52318be8aa7054
Artur Taranchiev <ewok@ewok.ru>
2019-05-06 14:58:55 +0300

    Some installation improvements

--- install.sh
+++ install.sh
@@ -1,20 +1,45 @@
-set -x
+#!/bin/bash
+
+set -xe
+
+if [[ -d $HOME/.tmux/resurrect ]]; then rm -rf $HOME/.tmux/resurrect;fi
 
 git clone https://github.com/tmux-plugins/tmux-resurrect $HOME/.tmux/resurrect
+
+if [ -d $HOME/.oh-my-zsh ]; then rm -rf $HOME/.oh-my-zsh;fi
+
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+if [ -d $HOME/bin ]; then rm -rf $HOME/bin;fi
+
 git clone https://github.com/ewok/bin.git $HOME/bin
 
-ln -s ${PWD}/.tmux.conf $HOME/.tmux.conf
-ln -s ${PWD}/.zshrc $HOME/.zshrc
-ln -s ${PWD}/.vim $HOME/.vim
+function update()
+{
+    if [ -f $2 ]
+    then
+        rm -f $2
+    elif [ -d $2 ]
+    then
+        rm -rf $2
+    else
+        echo "Couldn't delete $2"
+        exit 1
+    fi
+
+    ln -s $1 $2
+}
+
+update ${PWD}/.tmux.conf $HOME/.tmux.conf
+update ${PWD}/.zshrc $HOME/.zshrc
+update ${PWD}/.vim $HOME/.vim
 mkdir -p $HOME/.config
-ln -s ${PWD}/.vim $HOME/.config/nvim
-ln -s ${PWD}/.zsh $HOME/.zsh
-ln -s ${PWD}/.ideavimrc $HOME/.ideavimrc
-ln -s ${PWD}/muttrc $HOME/.muttrc
-ln -s ${PWD}/mutt $HOME/.mutt
-ln -s ${PWD}/.vifm $HOME/.vifm
-rm -rf $HOME/.i3 || true
-ln -s ${PWD}/.i3 $HOME/.i3
-ln -s ${PWD}/.ctags $HOME/.ctags
-ln -s ${PWD}/.i3status.conf $HOME/.i3status.conf
+update ${PWD}/.vim $HOME/.config/nvim
+update ${PWD}/.zsh $HOME/.zsh
+update ${PWD}/.ideavimrc $HOME/.ideavimrc
+update ${PWD}/muttrc $HOME/.muttrc
+update ${PWD}/mutt $HOME/.mutt
+update ${PWD}/.vifm $HOME/.vifm
+update ${PWD}/.i3 $HOME/.i3
+update ${PWD}/.ctags $HOME/.ctags
+update ${PWD}/.i3status.conf $HOME/.i3status.conf
