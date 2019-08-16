7d1eb47b31d7cc9c5924dd50ea58ee0dc049aaa0
Artur Taranchiev <ewok@ewok.ru>
2019-08-16 15:45:28 +0300

    Add ssh wrapper

---
+++ .zsh/custom/ssh.pass.zsh
@@ -0,0 +1,14 @@
+function ssh(){
+    host=$1;
+    unset PASSWORD
+    unset PASSVAR
+    eval $(awk "/#[Pp]assvar / && inhost { printf \"PASSVAR=%s\",\$2; exit 1 } /#[Pp]assword / && inhost { printf \"PASSWORD=%s\",\$2; } /^[Hh][oO][sS][tT] / && inhost { inhost=0; exit 1 } /^[Hh][oO][sS][tT] $host\$/ { inhost=1 }" ~/.ssh/config)
+    if [[ -z "$PASSWORD" ]] && [[ -z "$PASSVAR" ]]; then
+        /usr/bin/ssh -q $*
+    else
+       if [[ -n "$PASSVAR" ]]; then
+          PASSWORD=$(TMP=${!PASSVAR-*};echo ${TMP##*-})
+       fi
+       $(command -v sshpass) -p"$PASSWORD" /usr/bin/ssh -q $*
+    fi
+}
