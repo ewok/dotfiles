00c81088a368d5aecb4a174ab5fd35d75e59a2f3
Artur Taranchiev <ewok@ewok.ru>
2019-08-17 09:55:48 +0300

    Update ssh wrapper

--- .zsh/custom/ssh.pass.zsh
+++ .zsh/custom/ssh.pass.zsh
@@ -4,11 +4,11 @@ function ssh(){
     unset PASSVAR
     eval $(awk "/#[Pp]assvar / && inhost { printf \"PASSVAR=%s\",\$2; exit 1 } /#[Pp]assword / && inhost { printf \"PASSWORD=%s\",\$2; } /^[Hh][oO][sS][tT] / && inhost { inhost=0; exit 1 } /^[Hh][oO][sS][tT] $host\$/ { inhost=1 }" ~/.ssh/config)
     if [[ -z "$PASSWORD" ]] && [[ -z "$PASSVAR" ]]; then
-        /usr/bin/ssh -q $*
+        /usr/bin/ssh $*
     else
        if [[ -n "$PASSVAR" ]]; then
           PASSWORD=$(TMP=${!PASSVAR-*};echo ${TMP##*-})
        fi
-       $(command -v sshpass) -p"$PASSWORD" /usr/bin/ssh -q $*
+       $(command -v sshpass) -p"$PASSWORD" /usr/bin/ssh $*
     fi
 }
