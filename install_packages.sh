3ecaa36988553dd59701f775c6b93b271c5e6974
Artur Taranchiev <artur.taranchiev@five9.com>
2019-10-16 17:12:58 +0300

    MUTT: Small changes

--- install_packages.sh
+++ install_packages.sh
@@ -6,4 +6,4 @@ ripgrep zsh-autosuggestions restic trash-cli \
 siji-git ttf-unifont nerd-fonts-noto-sans-mono \
 flameshot todoist-add-git nvme-cli todoist-linux-bin \
 slack-desktop redshift-gtk-git zeal npm ruby xclip \
-xsel par
+xsel par dante mbsync msmtp neomutt
