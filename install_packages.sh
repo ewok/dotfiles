d7dad48dc2dbdd0d10b15449f7bdc51e08fb8c93
Artur Taranchiev <artur.taranchiev@five9.com>
2019-11-01 11:17:56 +0300

    Hide yay diffmenu and cleanmenu

--- install_packages.sh
+++ install_packages.sh
@@ -1,9 +1,10 @@
 sudo pacman -Rsc vim palemoon-bin || echo "Probably absent."
-yay -S --needed gcc xst vifm neovim albert tmux python2 \
-python3 python-virtualenv ctags global unzip \
-ripgrep zsh-autosuggestions restic trash-cli \
-siji-git ttf-unifont nerd-fonts-noto-sans-mono \
-flameshot todoist-add-git nvme-cli todoist-linux-bin \
-slack-desktop redshift-gtk-git zeal npm ruby xclip \
-xsel par mbsync msmtp neomutt notmuch notmuch-mutt \
-davmail goobook lbdb vcal cht.sh
+yay -Syu --nocleanmenu --nodiffmenu --needed \
+    gcc xst vifm neovim albert tmux python2 \
+    python3 python-virtualenv ctags global unzip \
+    ripgrep zsh-autosuggestions restic trash-cli \
+    siji-git ttf-unifont nerd-fonts-noto-sans-mono \
+    flameshot todoist-add-git nvme-cli todoist-linux-bin \
+    slack-desktop redshift-gtk-git zeal npm ruby xclip \
+    xsel par mbsync msmtp neomutt notmuch notmuch-mutt \
+    davmail goobook lbdb vcal cht.sh
