6eba705d7f8773c396ac3b99521152949643a713
Artur Taranchiev <ewok@ewok.ru>
2019-11-17 22:09:46 +0300

    VIFM: Change pwd after exit to current

---
+++ config/.zsh/custom/vifm.zsh
@@ -0,0 +1,9 @@
+vifm()
+{
+    local dst="$(command vifm --choose-dir - "$@")"
+    if [ -z "$dst" ]; then
+        echo 'Directory picking cancelled/failed'
+        return 1
+    fi
+    cd "$dst"
+}
