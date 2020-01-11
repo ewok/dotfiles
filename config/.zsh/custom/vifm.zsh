9bc13b46519bc319ef9310cac81536f4335dee35
Artur Taranchiev <ewok@ewok.ru>
2020-01-12 01:07:49 +0300

    VIFM: Return and fix image preview(ueberzug)

--- config/.zsh/custom/vifm.zsh
+++ config/.zsh/custom/vifm.zsh
@@ -1,9 +1,16 @@
 vifm()
 {
-    local dst="$(command vifm --choose-dir - "$@")"
+    local dst="$(vifmrun "$@" | awk -F: '/PATH:/{print $2}')"
     if [ -z "$dst" ]; then
         echo 'Directory picking cancelled/failed'
         return 1
     fi
     cd "$dst"
+    echo "Ueberzug processes count: $(pgrep ueberzug | wc -l)"
+    if [ -z "$(pgrep vifm)" ]; then
+        rm -rf "/tmp/vifm_img";
+        echo "Cache was cleaned."
+    else
+        echo "Skip cleaning cache(Other vifm is running)."
+    fi
 }
