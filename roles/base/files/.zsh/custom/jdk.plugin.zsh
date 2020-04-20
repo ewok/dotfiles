0baebc955a7393d9fbe5a63e9a36f7dfae36088f
Artur Taranchiev <artur.taranchiev@five9.com>
2020-04-20 14:31:19 +0300

    Adapt setjdk to linux

--- roles/base/files/.zsh/custom/jdk.plugin.zsh
+++ roles/base/files/.zsh/custom/jdk.plugin.zsh
@@ -1,16 +1,18 @@
 function setjdk() {
   if [ $# -ne 0 ]; then
-   removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
-   if [ -n "${JAVA_HOME+x}" ]; then
-    removeFromPath $JAVA_HOME
-   fi
-   export JAVA_HOME=`/usr/libexec/java_home -v $@`
-   export PATH=$JAVA_HOME/bin:$PATH
+    removeFromPath '/usr/lib/jvm/default/bin'
+    if [ -n "${JAVA_HOME+x}" ]; then
+      removeFromPath $JAVA_HOME
+    fi
+    export JAVA_HOME="/usr/lib/jvm/$@"
+    export PATH=$JAVA_HOME/bin:$PATH
+
+    java -version
   fi
- }
- function removeFromPath() {
+}
+function removeFromPath() {
   export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
- }
+}
 
 # setjdk "1.7.0_80"
 # setjdk "1.8.0_51"
