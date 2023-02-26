2b7b9dab30991d41601cc409b5579ae40727932f
Artur Taranchiev <github@in.ewok.email>
2023-02-26 17:16:53 +0600

    flake is too heavy

--- packages/direnv/dotfiles/90_direnv_init.fish
+++ packages/direnv/dotfiles/90_direnv_init.fish
@@ -1,5 +1,12 @@
 if status --is-interactive; or status --is-login
   if command -vq -- direnv
     direnv hook fish | source
+    function __direnv_export_eval --on-event fish_prompt;
+        begin;
+            begin;
+                "/usr/bin/direnv" export fish
+            end 1>| source
+        end 2>| grep -E -v -e "^direnv: export"
+    end;
   end
 end
--- shell.nix
+++ shell.nix
@@ -17,16 +17,16 @@ pkgs.mkShell {
   shellHook = ''
     # PATH=${nix}/bin:$PATH
     echo *******************START*******************
-    # git-crypt-status
-    # curl -sL 'https://github.com/ewok/dotfiles/raw/main/local_vars.yaml' | md5sum -c local_vars.yaml.md5
-    # if [ $? -ne 0 ]; then
-    #   echo WARNING WARNING settings.yaml does not match hash!
-    #   echo -n "Expecting: "
-    #   cat local_vars.yaml.md5
-    #   echo -n "Got: "
-    #   curl -sL 'https://github.com/ewok/dotfiles/raw/main/local_vars.yaml' | md5sum
-    #   exit 1
-    # fi
+    git-crypt-status
+    curl -sL 'https://github.com/ewok/dotfiles/raw/main/local_vars.yaml' | md5sum -c local_vars.yaml.md5
+    if [ $? -ne 0 ]; then
+      echo WARNING WARNING settings.yaml does not match hash!
+      echo -n "Expecting: "
+      cat local_vars.yaml.md5
+      echo -n "Got: "
+      curl -sL 'https://github.com/ewok/dotfiles/raw/main/local_vars.yaml' | md5sum
+      exit 1
+    fi
     echo ********************END********************
   '';
 }
