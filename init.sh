152228856b826caba889988a01f29521daf40f4d
Artur Taranchiev <github@in.ewok.email>
2023-03-14 22:59:17 +0600

    no fish

--- init.sh
+++ init.sh
@@ -7,32 +7,28 @@ echo $ID
 
 case "$ID" in
     "pop"|"ubuntu"|"debian")
-        yes | sudo apt install ansible fish
+        yes | sudo apt install ansible
         ansible-galaxy collection install community.general
-        sudo usermod -s /usr/bin/fish $USER
     ;;
     "manjaro"|"manjaro-arm")
         sudo pacman -Syu --needed
         sudo pacman -S --needed yay
-        yay -S --needed ansible fish python-packaging
+        yay -S --needed ansible python-packaging
         ansible-galaxy collection install community.general
         ansible-galaxy collection install kewlfft.aur
-        sudo usermod -s /bin/fish $USER
     ;;
     "arcolinux")
         sudo pacman -Syu --needed
-        yay -S --needed ansible fish python-packaging
+        yay -S --needed ansible python-packaging
         ansible-galaxy collection install community.general
         ansible-galaxy collection install kewlfft.aur
-        sudo usermod -s /bin/fish $USER
     ;;
     "void")
         sudo xbps-install -Syu
-        sudo xbps-install -Sy ansible fish-shell
+        sudo xbps-install -Sy ansible
         sudo xbps-reconfigure -f glibc-locales
         ansible-galaxy collection install community.general
         ansible-galaxy collection install kewlfft.aur
-        sudo usermod -s /bin/fish $USER
     ;;
     *) echo No init.
     ;;
